713e11aeb908ef6611dbf23f8a554fdda5250e43
dnysaz <danayasa2@gmail.com>
2026-06-06 15:18:14 +0800

    feat: add WA_MESSAGE env var

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,7 @@
 export const PHONE = process.env.NEXT_PUBLIC_PHONE_NUMBER || "6285792721649";
 export const EMAIL = process.env.NEXT_PUBLIC_CONTACT_EMAIL || "halo@webkalcer.com";
 
-const msg = "Halo kak, saya mau buat website di webkalcer.com , bisa dibantu?";
-export const WA_URL = `https://wa.me/${PHONE}?text=${encodeURIComponent(msg)}`;
+const waMsg =
+  process.env.NEXT_PUBLIC_WA_MESSAGE ||
+  "Halo kak, saya mau buat website di webkalcer.com , bisa dibantu?";
+export const WA_URL = `https://wa.me/${PHONE}?text=${encodeURIComponent(waMsg)}`;
